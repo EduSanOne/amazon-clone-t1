6636fe1da1612aa296565f47840f361b526a2f30
EduSanOne <122896144+EduSanOne@users.noreply.github.com>
2026-03-09 01:06:50 -0300

    att

---
+++ script/amazon.js
@@ -0,0 +1,35 @@
+import { products , titles } from "../data/products.js";
+
+let productsboxHTML = '';
+
+
+// Create 8 box
+for(let i = 0; i < 8; i++){
+    productsboxHTML += `
+        <div class="products-box js-products-box-${i}" >
+            <div class="product-title-box">
+                <h3 class="product-title">${titles[i]}</h3>
+            </div>
+        </div>
+    `
+}
+
+document.querySelector('.grid-layout').innerHTML = productsboxHTML;
+
+
+let productHTML = '';
+
+// Add the 32 products
+for(let i = 0; i < 32; i++){
+    
+    const boxIndex = Math.floor(i / 4); // decides which box
+    
+    let image_att = products[i].image;
+    productHTML = `
+        <div class="product-card">
+            <img src="${image_att}" alt="" class="product">
+        </div>
+    `
+
+    document.querySelector(`.js-products-box-${boxIndex}`).innerHTML += productHTML;
+}
